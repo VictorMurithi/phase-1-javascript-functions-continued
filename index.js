f0e5e39ca0129a99f36539b1351d92916e6d1bf0
victor <victor.murithi@student.moringaschool.com>
2023-09-19 13:37:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = function(activity1="go to the office"){
+    return `This Monday, I will ${activity1}.`;
+}
+function wrapAdjective(string = "*"){
+    return function(param = "special"){
+        return `You are ${string}${param}${string}!`
+    }
+}
+const encouragingPromptFunction = wrapAdjective("%")("a dedicated programmer")
